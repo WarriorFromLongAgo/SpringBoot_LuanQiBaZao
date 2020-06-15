c3be50eda6c8481d0d168a9e208cf2573a1bffe1
WarriorFromLongAgo <hubeixuegao@gmail.com>
2020-06-15 11:22:28 +0800

    1

---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/Luanqibazao1Application.java
@@ -0,0 +1,13 @@
+package com.xuegao.luanqibazao_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Luanqibazao1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Luanqibazao1Application.class, args);
+    }
+
+}
---
+++ luanqibazao_1/src/test/java/com/xuegao/luanqibazao_1/Luanqibazao1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.luanqibazao_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Luanqibazao1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
