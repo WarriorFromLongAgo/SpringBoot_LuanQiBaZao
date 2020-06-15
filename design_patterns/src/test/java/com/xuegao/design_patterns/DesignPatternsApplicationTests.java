6edd11094ab85ca91d4deea54bc7bd274db1baee
WarriorFromLongAgo <hubeixuegao@gmail.com>
2020-06-15 17:47:43 +0800

    design patterns

---
+++ design_patterns/src/test/java/com/xuegao/design_patterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
