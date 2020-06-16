605cc0ec225b771c8ef246921862aad4a123de96
WarriorFromLongAgo <hubeixuegao@gmail.com>
2020-06-16 10:34:23 +0800

    nio

---
+++ nio_project/src/test/java/com/xuegao/nio_project/NioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.nio_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
