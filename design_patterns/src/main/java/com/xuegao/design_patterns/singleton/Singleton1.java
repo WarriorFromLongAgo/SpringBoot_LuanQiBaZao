fa5dc8ac867f529cc6ba4d3395d3c09993cfcd2f
WarriorFromLongAgo <hubeixuegao@gmail.com>
2020-06-23 18:31:11 +0800

    删除相关的设计模式

---
+++ design_patterns/src/main/java/com/xuegao/design_patterns/singleton/Singleton1.java
@@ -0,0 +1,14 @@
+package com.xuegao.design_patterns.singleton;
+
+/**
+ * <br/> @PackageName：com.xuegao.design_patterns.singleton
+ * <br/> @ClassName：Singleton1
+ * <br/> @Description：
+ * <br/> @author：花名：xuegao
+ * <br/> @date：2020/6/23 18:30
+ */
+public class Singleton1 {
+    private Singleton1(){
+
+    }
+}
