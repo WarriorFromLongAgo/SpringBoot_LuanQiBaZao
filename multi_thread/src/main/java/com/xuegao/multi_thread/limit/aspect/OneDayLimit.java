054ea14ae7098d912814f71f04ccc35d425058f8
WarriorFromLongAgo <hubeixuegao@gmail.com>
2020-07-01 10:12:35 +0800

    多线程高并发下的模块

---
+++ multi_thread/src/main/java/com/xuegao/multi_thread/limit/aspect/OneDayLimit.java
@@ -0,0 +1,43 @@
+package com.xuegao.multi_thread.limit.aspect;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * <br/> @PackageName：com.xuegao.xianliu_demo.aspect
+ * <br/> @ClassName：
+ * <br/> @Description：
+ * <br/> @author：花名：xuegao
+ * <br/> @date：2020/6/15 13:53
+ */
+public class OneDayLimit {
+
+    private LoadingCache<Long, LongAdder> counter =
+            CacheBuilder.newBuilder().expireAfterWrite(2, TimeUnit.SECONDS)
+                    .build(new CacheLoader<Long, LongAdder>() {
+                        @Override
+                        public LongAdder load(Long aLong) {
+                            return new LongAdder();
+                        }
+                    });
+    private static long permit = 50;
+
+    public ResponseEntity getData() throws ExecutionException {
+        long currentTime = System.currentTimeMillis() / 1000;
+        LongAdder longAdder = counter.get(currentTime);
+        longAdder.increment();
+        if (longAdder.sum() > permit) {
+            return ResponseEntity.of(Optional.of("访问速度过快"));
+        }
+        // 业务处理
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ multi_thread/src/test/java/com/xuegao/multi_thread/MultiThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.multi_thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
