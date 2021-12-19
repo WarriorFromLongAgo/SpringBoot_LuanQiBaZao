06d0160120905c05f623513a4c96803051ab6fa1
WarriorFromLongAgo <hubeixuegao@gmail.com>
2021-12-19 21:33:04 +0800

    init

---
+++ video_office_audio/src/main/java/com/xuegao/video_conver/txt_to_audio/AudioClip.java
@@ -0,0 +1,14 @@
+package com.xuegao.video_conver.txt_to_audio;
+
+/**
+ * @author xuegao
+ * @version 1.0
+ * @date 2021/12/19 20:31
+ */
+public class AudioClip {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ video_office_audio/src/main/java/com/xuegao/video_conver/txt_to_audio/BaiduTxtToAudio.java
@@ -0,0 +1,19 @@
+package com.xuegao.video_conver.txt_to_audio;
+
+/**
+ * @author xuegao
+ * @version 1.0
+ * @date 2021/12/19 20:59
+ */
+public class BaiduTxtToAudio {
+
+
+    private static final String app_key = "UcMThVgRvUs5rLojwfYfjCGF";
+    private static final String App_ID = "UcMThVgRvUs5rLojwfYfjCGF";
+    private static final String Secret_Key = "UcMThVgRvUs5rLojwfYfjCGF";
+
+    public static void main(String[] args) {
+
+
+    }
+}
