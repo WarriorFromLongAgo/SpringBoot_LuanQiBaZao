d916a8342e75977380aa02fe4812b79f1c3ecdf9
fjm <593436@ky-tech.com.cn>
2022-01-11 21:20:22 +0800

    hash

---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/jdk8/lang/string/HashCode.java
@@ -0,0 +1,16 @@
+package com.xuegao.luanqibazao_1.jdk8.lang.string;
+
+/**
+ *
+ * @author xuegao
+ * @version 1.0
+ * @date 2022/1/11 21:18
+ */
+public class HashCode {
+    public static void main(String[] args) {
+        String aaa = "aaa";
+        String bbb = "撒娇撒很简单";
+        System.out.println(aaa.hashCode());
+        System.out.println(bbb.hashCode());
+    }
+}
