c65f8bbae6529ca59686f0c270288cda1f7dd88e
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-01-28 12:52:52 +0800

    design

---
+++ design_patterns/src/main/java/com/xuegao/design_patterns/xingwei/chain/ss.java
@@ -0,0 +1,9 @@
+package com.xuegao.design_patterns.xingwei.chain;
+
+/**
+ * @author xuegao
+ * @version 1.0
+ * @date 2022/1/28 12:51
+ */
+public class ss {
+}
---
+++ design_patterns/src/main/java/com/xuegao/design_patterns/xingwei/proxy/sss.java
@@ -0,0 +1,9 @@
+package com.xuegao.design_patterns.xingwei.proxy;
+
+/**
+ * @author xuegao
+ * @version 1.0
+ * @date 2022/1/28 12:51
+ */
+public class sss {
+}
