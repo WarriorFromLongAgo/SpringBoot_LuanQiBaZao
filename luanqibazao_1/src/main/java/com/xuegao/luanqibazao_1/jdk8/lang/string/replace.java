b92c099bc54f0e2e49b931c6895c383d67d54c8c
feijiumeng <593436@ky-tech.com.cn>
2022-02-26 14:54:29 +0800

    design

--- luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/jdk8/lang/string/replace.java
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/jdk8/lang/string/replace.java
@@ -1,6 +1,14 @@
 package com.xuegao.luanqibazao_1.jdk8.lang.string;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class replace {
+    // String str = "--/鞍山市/--/辽阳市/--";
+    //     System.out.println(str);
+    // str = replace(str, "--/", "");
+    //     System.out.println(str);
+    // str = replace(str, "/--", "");
+    //     System.out.println(str);
     public static String replace(String str, String oldStr, String newStr) {
         // String area = "--/鞍山市/--/辽阳市/--";
         // System.out.println(area);
@@ -13,12 +21,27 @@ public class replace {
         return str.replaceAll(oldStr, newStr);
     }
 
+    public static String replace(String str, String newStr, String... oldStrArr) {
+        if (StringUtils.isAllBlank(str)) {
+            return str;
+        }
+        // String area = "--/鞍山市/--/辽阳市/--";
+        // System.out.println(area);
+        // area = area.replace("--/", "");
+        // System.out.println(area);
+        // area = area.replace("/--", "");
+        // str = str.replace(oldStr, newStr);
+        // System.out.println(area);
+        for (String oldStr : oldStrArr) {
+            str = str.replace(oldStr, newStr);
+        }
+        return str;
+    }
+
     public static void main(String[] args) {
         String str = "--/鞍山市/--/辽阳市/--";
         System.out.println(str);
-        str = replace(str, "--/", "");
-        System.out.println(str);
-        str = replace(str, "/--", "");
+        str = replace(str, "", "--/", "/--");
         System.out.println(str);
     }
 
