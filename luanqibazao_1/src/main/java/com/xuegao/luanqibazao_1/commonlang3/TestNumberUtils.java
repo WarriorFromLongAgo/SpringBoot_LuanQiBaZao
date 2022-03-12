ae48e81b32fe286bd565568cb3e3849f96f2c3fd
feijiumeng <593436@ky-tech.com.cn>
2022-03-12 11:07:12 +0800

    commonlang3

---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/commonlang3/TestNumberUtils.java
@@ -0,0 +1,47 @@
+package com.xuegao.luanqibazao_1.commonlang3;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * @author xuegao
+ * @version 1.0
+ * @date 2022/3/12 10:59
+ */
+public class TestNumberUtils {
+    public static void main(String[] args) {
+        String str1 = "123.456";
+        String str2 = "-123.456";
+        String str3 = "123";
+        String str4 = "-123";
+
+        System.out.println("str1 = " + str1);
+        System.out.println("NumberUtils.isCreatable(str1) = " + NumberUtils.isCreatable(str1));
+        System.out.println("NumberUtils.toInt(str1) = " + NumberUtils.toInt(str1));
+        System.out.println("NumberUtils.isDigits(str1) = " + NumberUtils.isDigits(str1));
+        System.out.println("NumberUtils.isParsable(str1) = " + NumberUtils.isParsable(str1));
+
+        System.out.println("==============================================");
+
+        System.out.println("str2 = " + str2);
+        System.out.println("NumberUtils.isCreatable(str2) = " + NumberUtils.isCreatable(str2));
+        System.out.println("NumberUtils.toInt(str2) = " + NumberUtils.toInt(str2));
+        System.out.println("NumberUtils.isDigits(str2) = " + NumberUtils.isDigits(str2));
+        System.out.println("NumberUtils.isParsable(str2) = " + NumberUtils.isParsable(str2));
+
+        System.out.println("==============================================");
+
+        System.out.println("str3 = " + str3);
+        System.out.println("NumberUtils.isCreatable(str3) = " + NumberUtils.isCreatable(str3));
+        System.out.println("NumberUtils.toInt(str3) = " + NumberUtils.toInt(str3));
+        System.out.println("NumberUtils.isDigits(str3) = " + NumberUtils.isDigits(str3));
+        System.out.println("NumberUtils.isParsable(str3) = " + NumberUtils.isParsable(str3));
+
+        System.out.println("==============================================");
+
+        System.out.println("str4 = " + str4);
+        System.out.println("NumberUtils.isCreatable(str4) = " + NumberUtils.isCreatable(str4));
+        System.out.println("NumberUtils.toInt(str4) = " + NumberUtils.toInt(str4));
+        System.out.println("NumberUtils.isDigits(str4) = " + NumberUtils.isDigits(str4));
+        System.out.println("NumberUtils.isParsable(str4) = " + NumberUtils.isParsable(str4));
+    }
+}
---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/commonlang3/TestStringUtils.java
@@ -0,0 +1,12 @@
+package com.xuegao.luanqibazao_1.commonlang3;
+
+/**
+ * @author xuegao
+ * @version 1.0
+ * @date 2022/3/12 11:05
+ */
+public class TestStringUtils {
+    public static void main(String[] args) {
+        
+    }
+}
