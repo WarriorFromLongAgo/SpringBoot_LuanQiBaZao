d98dd9f41e3ac7f1dc05ca7fcf1ed61e4e80ee28
feijiumeng <593436@ky-tech.com.cn>
2022-04-09 17:26:58 +0800

    commonlang3

--- multi_thread2/src/main/java/com/xuegao/multi_thread2/juc/completablefuture/thenAcceptBoth1.java
+++ multi_thread2/src/main/java/com/xuegao/multi_thread2/juc/completablefuture/thenAcceptBoth1.java
@@ -2,6 +2,8 @@ package com.xuegao.multi_thread2.juc.completablefuture;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 /**
  * <br/> @PackageName：com.xuegao.multi_thread2.juc.completablefuture
@@ -13,7 +15,17 @@ import java.util.concurrent.ExecutionException;
 public class thenAcceptBoth1 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // 处理异步计算结果
-        CompletableFuture completableFuture4 = CompletableFuture.supplyAsync(() -> "Hello")
+        CompletableFuture completableFuture4 = CompletableFuture.supplyAsync(new Supplier<String>() {
+            @Override
+            public String get() {
+                try {
+                    TimeUnit.SECONDS.sleep(5L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "hello";
+            }
+        })
                 .thenAcceptBoth(CompletableFuture.supplyAsync(() -> " World 4"),
                         (s1, s2) -> System.out.println(s1 + s2));
         System.out.println(completableFuture4.get());
