08fb777a270d0adb297de9a474a41de733a9575c
fjm <Xuegaoyu3>
2022-05-18 10:20:40 +0800

    51

---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/test/For.java
@@ -0,0 +1,13 @@
+package com.xuegao.luanqibazao_1.test;
+
+public class For {
+    public static void main(String[] args) {
+        System.out.println("-----------------");
+        System.out.println("-----------------");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(" i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
