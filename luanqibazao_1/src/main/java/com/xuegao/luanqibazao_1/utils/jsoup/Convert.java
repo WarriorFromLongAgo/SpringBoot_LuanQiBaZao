cc5f9903f2fafe0e614e36052761d9016143fe91
fjm <593436@ky-tech.com.cn>
2022-06-08 09:59:20 +0800

    convert

---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/utils/jsoup/Convert.java
@@ -0,0 +1,9 @@
+package com.xuegao.luanqibazao_1.utils.jsoup;
+
+public class Convert {
+    public FileNameUrlV2 convert(FileNameUrl fileNameUrl){
+        FileNameUrlV2 fileNameUrlV2 = new FileNameUrlV2();
+
+        return fileNameUrlV2;
+    }
+}
