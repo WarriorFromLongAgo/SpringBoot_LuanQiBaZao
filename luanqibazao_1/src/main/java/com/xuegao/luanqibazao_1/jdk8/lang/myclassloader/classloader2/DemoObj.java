c2a39ecb0de15b26db00eb6feb90584897a9c58e
593436 <Xuegaoyu3>
2022-09-02 14:09:11 +0800

    classloader

---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/jdk8/lang/myclassloader/classloader2/DemoObj.java
@@ -0,0 +1,10 @@
+package com.xuegao.luanqibazao_1.jdk8.lang.myclassloader.classloader2;
+
+public class DemoObj {
+
+    @Override
+    public String toString() {
+        return "I am DemoObj";
+    }
+
+}
