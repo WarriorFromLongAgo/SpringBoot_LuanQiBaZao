ca73661b67b0cd058722e637d60081b76767ed33
feijiumeng <Xuegaoyu3>
2022-11-07 12:05:10 +0800

    jackson

---
+++ luanqibazao_1/src/main/java/com/xuegao/luanqibazao_1/commonlang3/objectutil/TestObjectUtils.java
@@ -0,0 +1,18 @@
+package com.xuegao.luanqibazao_1.commonlang3.objectutil;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+/**
+ * @author fjm
+ * @date 2022年11月07日 12:04
+ */
+public class TestObjectUtils {
+
+    public static void (){
+        // ObjectUtils.allNotNull()
+
+
+    }
+
+
+}
